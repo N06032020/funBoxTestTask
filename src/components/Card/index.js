c740c2fdefb319d01205efebd05d807d780722af
Nick <zzz727272@gmail.com>
2022-06-18 15:01:57 +0300

    card bottom fix

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -38,19 +38,7 @@ function Card() {
       <div className={styles.container}>
         <div className={styles.cardsItems}>
           {/* First card */}
-          <div className={isActive ? styles.cardItem : styles.cardItemActive} onClick={toggleClass}>
-            <div className={styles.cardItemTextContent}>
-              <p className={styles.cardItemTextFirst}>Сказочное заморское яство</p>
-              <h1 className={styles.cardItemTextSecond}>Нямушка</h1>
-              <p className={styles.cardItemTextThird}>с фуа-гра</p>
-              <p className={styles.cardItemTextFourth}>
-                10 порций <br /> мышь в подарок
-              </p>
-            </div>
-            <div className={styles.weightRound}>
-              <span className={styles.weightRoundNumbers}>0,5</span>
-              <span className={styles.weightRoundText}>кг</span>
-            </div>
+          <div className={styles.cardWrapper}>
             <p
               className={styles.cardBottom}
               onClick={() =>
@@ -58,24 +46,26 @@ function Card() {
               }>
               {buttonText} <span>{buttonTextSecond}</span>
             </p>
+            <div
+              className={isActive ? styles.cardItem : styles.cardItemActive}
+              onClick={toggleClass}>
+              <div className={styles.cardItemTextContent}>
+                <p className={styles.cardItemTextFirst}>Сказочное заморское яство</p>
+                <h1 className={styles.cardItemTextSecond}>Нямушка</h1>
+                <p className={styles.cardItemTextThird}>с фуа-гра</p>
+                <p className={styles.cardItemTextFourth}>
+                  10 порций <br /> мышь в подарок
+                </p>
+              </div>
+              <div className={styles.weightRound}>
+                <span className={styles.weightRoundNumbers}>0,5</span>
+                <span className={styles.weightRoundText}>кг</span>
+              </div>
+            </div>
           </div>
 
           {/* Second card */}
-          <div
-            className={isActiveFish ? styles.cardItem : styles.cardItemActive}
-            onClick={toggleClassFish}>
-            <div className={styles.cardItemTextContent}>
-              <p className={styles.cardItemTextFirst}>Сказочное заморское яство</p>
-              <h1 className={styles.cardItemTextSecond}>Нямушка</h1>
-              <p className={styles.cardItemTextThird}>с рыбой</p>
-              <p className={styles.cardItemTextFourth}>
-                40 порций <br /> 2 мыши в подарок
-              </p>
-            </div>
-            <div className={styles.weightRound}>
-              <span className={styles.weightRoundNumbers}>2</span>
-              <span className={styles.weightRoundText}>кг</span>
-            </div>
+          <div className={styles.cardWrapper}>
             <p
               className={styles.cardBottom}
               onClick={() =>
@@ -86,25 +76,25 @@ function Card() {
               }>
               {buttonTextFish} <span>{buttonTextSecondFish}</span>
             </p>
+            <div
+              className={isActiveFish ? styles.cardItem : styles.cardItemActive}
+              onClick={toggleClassFish}>
+              <div className={styles.cardItemTextContent}>
+                <p className={styles.cardItemTextFirst}>Сказочное заморское яство</p>
+                <h1 className={styles.cardItemTextSecond}>Нямушка</h1>
+                <p className={styles.cardItemTextThird}>с рыбой</p>
+                <p className={styles.cardItemTextFourth}>
+                  40 порций <br /> 2 мыши в подарок
+                </p>
+              </div>
+              <div className={styles.weightRound}>
+                <span className={styles.weightRoundNumbers}>2</span>
+                <span className={styles.weightRoundText}>кг</span>
+              </div>
+            </div>
           </div>
           {/* Third card */}
-          <div
-            className={isActiveChicken ? styles.cardItem : styles.cardItemActive}
-            onClick={toggleClassChicken}>
-            <div className={styles.cardItemTextContent}>
-              <p className={styles.cardItemTextFirst}>Сказочное заморское яство</p>
-              <h1 className={styles.cardItemTextSecond}>Нямушка</h1>
-              <p className={styles.cardItemTextThird}>с курой</p>
-              <p className={styles.cardItemTextFourth}>
-                100 порций
-                <br />5 мышей в подарок
-                <br /> заказчик доволен
-              </p>
-            </div>
-            <div className={styles.weightRound}>
-              <span className={styles.weightRoundNumbers}>5</span>
-              <span className={styles.weightRoundText}>кг</span>
-            </div>
+          <div className={styles.cardWrapper}>
             <p
               className={styles.cardBottom}
               onClick={() =>
@@ -115,6 +105,24 @@ function Card() {
               }>
               {buttonTextChicken} <span>{buttonTextSecondChicken}</span>
             </p>
+            <div
+              className={isActiveChicken ? styles.cardItem : styles.cardItemActive}
+              onClick={toggleClassChicken}>
+              <div className={styles.cardItemTextContent}>
+                <p className={styles.cardItemTextFirst}>Сказочное заморское яство</p>
+                <h1 className={styles.cardItemTextSecond}>Нямушка</h1>
+                <p className={styles.cardItemTextThird}>с курой</p>
+                <p className={styles.cardItemTextFourth}>
+                  100 порций
+                  <br />5 мышей в подарок
+                  <br /> заказчик доволен
+                </p>
+              </div>
+              <div className={styles.weightRound}>
+                <span className={styles.weightRoundNumbers}>5</span>
+                <span className={styles.weightRoundText}>кг</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
